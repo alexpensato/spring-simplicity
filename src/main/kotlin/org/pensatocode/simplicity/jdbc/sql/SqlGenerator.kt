4ca7f867742a8240e17687f15501ae412eaa8505
alexpensato <alex@pensato.com.br>
2020-04-14 10:00:59 -0300

    Update contact info

--- src/main/kotlin/org/pensatocode/simplicity/jdbc/sql/SqlGenerator.kt
+++ src/main/kotlin/org/pensatocode/simplicity/jdbc/sql/SqlGenerator.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 twitter.com/PensatoAlex
+ * Copyright 2017-2020 Alex Magalhaes <alex@pensatocode.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
